1130bf5e71101deb308b818c3279bd303eeac3e0
Lingabathula Sharath <sharathlingabathula@gmail.com>
2023-05-05 19:03:31 +0530

    Added codes

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const connect = require('./config/database');
+
+const app = express();
+
+const TweetRepository = require('./repository/tweet-repository')
+const Comment = require('./models/comment');
+
+app.listen(3000, async() =>{
+     console.log('Server Started');
+     await connect();
+     console.log('Mongo db connected');
+
+    /* const tweet = await Tweet.create({
+          content: 'Third tweet',
+          userEmail: 'a@b.com'
+     })
+     */
+    const tweetRepo = new TweetRepository;
+    /*const tweet = await tweetRepo.create({content: 'Tweet with comment schema'});
+    console.log(tweet);
+    const comment = await Comment.create({content: 'new comment'});
+    tweet.comments.push(comment);
+    await tweet.save();
+     console.log(tweet);
+     */
+    const tweet = await tweetRepo.getWithComments('6454fc2e63efcc6b7da2c024');
+    console.log(tweet);
+});
