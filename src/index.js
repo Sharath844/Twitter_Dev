c9944d8208206dc4d1352adc637a4f22f88c0af4
Lingabathula Sharath <sharathlingabathula@gmail.com>
2023-05-12 13:00:20 +0530

    Added passport logic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import passport from 'passport';
+
 import {connect} from './config/database.js';
 
-import { TweetRepository, UserRepository, LikeRepository, CommentRepository} from './repository/index.js';
-import LikeService from './services/like-service.js';
-import CommentService from './services/comment-service.js';
+import { passportAuth } from './config/jwt-middleware.js';
 
 import apiRoutes from './routes/index.js';
 
@@ -12,8 +12,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/api', apiRoutes);
+app.use(passport.initialize());
+passportAuth(passport);
 
+app.use('/api', apiRoutes);
 
 
 app.listen(3000, async() =>{
@@ -21,29 +23,6 @@ app.listen(3000, async() =>{
      await connect();
      console.log('Mongo db connected');
      
-     
-     const tweetRepo = new TweetRepository();
-     const userRepo = new UserRepository();
-     
-     
-     const tweets = await tweetRepo.getAll(0, 10);
     
-     const users = await userRepo.getAll(); 
-     /*
-     const likeService = new LikeService();
-     const response = await likeService.toggleLike(tweets[0].id, 'Tweet', users[0].id); 
-    
-    const userRepo = new UserRepository();
-     const user = await userRepo.create({
-          email: 'Sharath@admin.com',
-          password: 'Sharath1234',
-          name: 'Sharath'
-     })
-     */
-
-
-     const commentService = new CommentService();
-     const response = await commentService.create(tweets[0].id, 'Tweet', users[0].id, 'first comment on tweet model');
-      
     
 });
